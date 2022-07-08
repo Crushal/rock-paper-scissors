01027fa074d7b6ca295eb30983c4eaacf5d40456
Crushal <devsherif0@gmail.com>
2022-07-08 22:12:40 +0200

    Add play_round function

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ function computer_play() {
     game = ["Rock", "Paper", "Scissors"]
     return game[Math.floor(Math.random() * game.length)]
 }
+
+function play_round(player_selection, computer_selection) {
+    if(player_selection.toLowerCase() === "rock" && computer_selection.toLowerCase() === "scissors" ||
+    player_selection.toLowerCase() === "paper" && computer_selection.toLowerCase() === "rock" ||
+    player_selection.toLowerCase() === "scissors" && computer_selection.toLowerCase() === "paper") {
+        return `You Won! ${player_selection} beats ${computer_selection}`
+    }
+    return `You Lost! ${computer_selection} beats ${player_selection}`
+}
