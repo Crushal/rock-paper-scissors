320a5478965ab40b215efb8fc71d3b1b3c440ec6
Crushal <devsherif0@gmail.com>
2022-07-20 20:09:30 +0200

    everything done

--- main.js
+++ main.js
@@ -1,82 +1,90 @@
 function computerPlay() {
-    let choice = ["rock", "paper", "scissors"]
-    return choice[Math.floor(Math.random() * choice.length)]
+    let computer = ["rock", "paper", "scissors"]
+    let equal = computer[Math.floor(Math.random() * computer.length)]
+    return equal  
 }
+
 function singleRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection) {
+    if (playerSelection === computerSelection) {
         return "Draw!"
     }
-    else if (playerSelection.toLowerCase() === "rock") {
-        if (computerSelection === "scissors") {
+    else if(playerSelection == "rock"){
+        if (computerSelection == "scissors"){
             return true
         }
         else {
             return false
         }
     }
-    else if (playerSelection.toLowerCase() === "paper"){
-        if (computerSelection === "rock") {
+    else if(playerSelection == "paper"){
+        if(computerSelection == "rock"){
             return true
         }
         else {
             return false
         }
     }
-    else if (playerSelection.toLowerCase() === "scissors") {
-        if (computerSelection === "paper") {
+    else if(playerSelection == "scissors"){
+        if(computerSelection == "paper"){
             return true
         }
         else {
             return false
         }
+    }
+}
+
+function moreThanFive (playerScore, computerScore){
+    if(playerScore >= 5 || computerScore >= 5) {
+        return true
+    }
+    return false
+}
+
+let playerScore = 0
+let computerScore = 0
+
+const scoreBoard = document.querySelector(".scoreBoard")
+const computerChoice = document.querySelector(".computerChoice")
+const playerChoice = document.querySelector(".playerChoice")
+const decider = document.querySelector(".decider")
+
+scoreBoard.textContent = `Player Score : ${playerScore} | Computer Score : ${computerScore}`
+
+function game (result) {
+    if (result == true) {
+        decider.textContent = "You Won!"
+        playerScore++
+        scoreBoard.textContent = `Player Score : ${playerScore} | Computer Score : ${computerScore}`
+    }
+    else if (result == false) {
+        decider.textContent = "You Lost!"
+        computerScore++
+        scoreBoard.textContent = `Player Score : ${playerScore} | Computer Score : ${computerScore}`
+    }
+    else {
+        decider.textContent = "Draw!"
     }
 }
 
 const rock = document.querySelector(".rock")
-rock.addEventListener("click", function(e){
-    singleRound("rock", computerPlay())
-    console.log("rock")
+rock.addEventListener("click", function(e) {
+    result = singleRound("rock", computerPlay())
+    computerChoice.textContent = computerPlay()
+    game(result)
 })
 
 const paper = document.querySelector(".paper")
-paper.addEventListener("click", function(e){
-    singleRound("paper", computerPlay())
-    console.log("paper")
+paper.addEventListener("click", function(e) {
+    result = singleRound("paper", computerPlay())
+    computerChoice.textContent = computerPlay()
+    game(result)
 })
 
 const scissors = document.querySelector(".scissors")
-scissors.addEventListener("click", function(e){
-    singleRound("scissors", computerPlay())
-    console.log("scissors")
+scissors.addEventListener("click", function(e) {
+    result = singleRound("scissors", computerPlay())
+    computerChoice.textContent = computerPlay()
+    game(result)
 })
 
-
-// const paper = document.querySelector(".paper")
-// paper.addEventListener("click", function(e){
-//     singleRound("paper", computerPlay())
-// })
-
-
-// const scissors = document.querySelector(".scissors")
-// scissors.addEventListener("click", function(e){
-//     singleRound("scissors", computerPlay())
-// })
-// function game() {
-//     let playerScore = 0
-//     let computerScore = 0
-//     for (let i = 0; i < 5; i++){
-//         let computer = computerPlay()
-//         let player = prompt("Rock, Paper or Scissors:")
-//         let oneRound = singleRound(player, computer)
-//         console.log(player)
-//         console.log(computer)
-//         console.log(oneRound)
-//         if (oneRound === true) {
-//             playerScore++
-//         }
-//         else if (oneRound === false) {
-//             computerScore++
-//         }
-//     }
-//     return [playerScore, computerScore]
-// }
